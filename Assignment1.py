9286ed11c173335ac28986a25e2583db48c0b9ea
Ankush Bhateja <128409237+ankushbhateja@users.noreply.github.com>
2025-05-21 01:11:34 +0530

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,25 +1,24 @@
-# Question 1
-# a = int(input("Enter the first number: "))
-# b = int(input("Enter the second number: "))
+# Task 1: Perform Basic Mathematical Operations
 
-# Question 2
-# 2.1  Addition
-# c = a+b
-# print(f'Number1 + Number 2 will be {c}')
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
 
-# # 2.2  Subtraction
-# c = a-b
-# print(f'Number1 - Number 2 will be {c}')
+c = a+b
+print(f'Number1 + Number 2 will be {c}')
 
-# # 2.3  Multiplication
-# c = a*b
-# print(f'Number1 * Number 2 will be {c}')
+# 2.2  Subtraction
+c = a-b
+print(f'Number1 - Number 2 will be {c}')
 
-# # 2.4 Division
-# c = a/b
-# print(f'Number1 / Number 2 will be {c}')
+# 2.3  Multiplication
+c = a*b
+print(f'Number1 * Number 2 will be {c}')
 
-# Task 2: Create a Personalized Greeting
+# 2.4 Division
+c = a/b
+print(f'Number1 / Number 2 will be {c}')
+
+# Task 2: Create a Personalised Greeting
 
 fname = input("Enter your first name:")
 lname = input("Enter your last name:")
