27dadbbeae05aabeebbd9867c090fe04c945be5b
Ankush Bhateja <128409237+ankushbhateja@users.noreply.github.com>
2025-05-21 01:09:09 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,29 @@
+# Question 1
+# a = int(input("Enter the first number: "))
+# b = int(input("Enter the second number: "))
+
+# Question 2
+# 2.1  Addition
+# c = a+b
+# print(f'Number1 + Number 2 will be {c}')
+
+# # 2.2  Subtraction
+# c = a-b
+# print(f'Number1 - Number 2 will be {c}')
+
+# # 2.3  Multiplication
+# c = a*b
+# print(f'Number1 * Number 2 will be {c}')
+
+# # 2.4 Division
+# c = a/b
+# print(f'Number1 / Number 2 will be {c}')
+
+# Task 2: Create a Personalized Greeting
+
+fname = input("Enter your first name:")
+lname = input("Enter your last name:")
+
+fullName = fname+ ' '+ lname
+
+print(f'Hello,{fullName}! Welcome to pyhton program')
